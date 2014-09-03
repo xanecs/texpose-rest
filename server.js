97a7f73c4764cf7835812d3ba10966297b3663ec
Leon Bentrup <leon@leonadi.de>
2014-09-03 19:47:32 +0200

    Always be corsing

--- server.js
+++ server.js
@@ -31,21 +31,14 @@ var server = restify.createServer({
 });
 
 function unknownMethodHandler(req, res) {
-    if (req.method.toLowerCase() === 'options') {
         //console.log('received an options method request');
         var allowHeaders = ['Accept', 'Accept-Version', 'Content-Type', 'Api-Version', 'Origin', 'X-Requested-With', 'Project', 'Token', 'Path']; // added Origin & X-Requested-With
-
-        if (res.methods.indexOf('OPTIONS') === -1) res.methods.push('OPTIONS');
-
         res.header('Access-Control-Allow-Credentials', true);
         res.header('Access-Control-Allow-Headers', allowHeaders.join(', '));
         res.header('Access-Control-Allow-Methods', res.methods.join(', '));
         res.header('Access-Control-Allow-Origin', req.headers.origin);
 
         return res.send(204);
-    }
-    else
-        return res.send(new restify.MethodNotAllowedError());
 }
 
 server.use(restify.fullResponse());
